3760150e13e86c4627244414857fdd0ddd449c0c
nfc916 <44668822@163.com>
2022-08-26 18:28:32 +0800

    init project

--- src/main/java/org/lanqiao/MainProcess.java
+++ src/main/java/org/lanqiao/MainProcess.java
@@ -1,5 +1,9 @@
 package org.lanqiao;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  *  1 目的 解析 SQL 文件 生成 markdown 表格
@@ -9,25 +13,37 @@ package org.lanqiao;
  *
  */
 public class MainProcess {
-
+    public static String filePath ="/home/project/db.sql";
     public static void main(String[] args) {
-
          startBuild(args);
+    }
 
+    private static void startBuild(String[] args) {
+        if(args!=null&&args.length>0){
+           //导出数据库内容成文件  数据库名，用户名，密码
+            System.out.println(" 导出数据库内容成文件 ");
+        }
+        // 直接将文件转成 MarkDown
+        //1 解析SQL语句
+        buildMarkdown(doParseSQL(filePath));
+        //2 将内容写成 文件
+
+    }
+
+    private static void buildMarkdown(List<String> sqls) {
+        try {
+            BufferedWriter bw =new BufferedWriter(new FileWriter(filePath));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
 
-    private static void startBuild(String[] args) {
-        if(args==null||args.length==0){
-
-        }else{
-            for(String s:args){
-
-
-            }
-        }
-
+    private static List<String> doParseSQL(String filePath) {
+        List<String> sqls =new ArrayList<>();
+        return sqls;
     }
 
 }
